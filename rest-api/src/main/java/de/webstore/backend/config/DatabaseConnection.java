8006021f7c2c426632fa66fb33d4fb76c173ab44
Davain Edwards <core8@gmx.net>
2024-03-03 21:44:38 +0100

    :shirt: Refactor DatabaseConnection.java and inject database settings from application.properties file

--- rest-api/src/main/java/de/webstore/backend/config/DatabaseConnection.java
+++ rest-api/src/main/java/de/webstore/backend/config/DatabaseConnection.java
@@ -3,23 +3,32 @@ package de.webstore.backend.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/warehouse_database_de?useSSL=false";
-    private static final String USER = "root";
-    private static final String PASSWORD = "password";
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
 
     static {
         try {
-            // Load the MySQL JDBC driver class
+            // Assuming you're using MySQL
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public static Connection getConnection() throws SQLException {
-        // Establish a connection to the MySQL database and return it
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    public Connection getConnection() throws SQLException {
+        // Use the injected properties to establish a connection
+        return DriverManager.getConnection(url, username, password);
     }
 }
