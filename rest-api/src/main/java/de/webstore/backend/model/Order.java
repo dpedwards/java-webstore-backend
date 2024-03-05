1c4d643ca207578b5944cf54727e0c06eda4e2fb
Davain Edwards <core8@gmx.net>
2024-03-05 01:44:29 +0100

    :pencil: Add comments to Order.java

--- rest-api/src/main/java/de/webstore/backend/model/Order.java
+++ rest-api/src/main/java/de/webstore/backend/model/Order.java
@@ -1,35 +1,69 @@
+/* Copyright Davain Pablo Edwards core8@gmx.net. Licensed https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en */
 package de.webstore.backend.model;
 
 import java.sql.Date;
 
+/**
+ * Represents an order entity in the system.
+ * <p>
+ * This class models the order entity with its attributes and provides getter and setter methods to manipulate its state.
+ */
 public class Order {
     
+    // Unique identifier for the order
     private int orderNumber;
+    
+    // The date the order was placed
     private Date date;
 
-    // Getter und Setter
+    /**
+     * Retrieves the order number.
+     *
+     * @return the order number
+     */
     public int getOrderNumber() {
         return orderNumber;
     }
 
-    public void setOrdernumber(int orderNumber) {
+    /**
+     * Sets the order number.
+     *
+     * @param orderNumber the new order number
+     */
+    public void setOrderNumber(int orderNumber) {
         this.orderNumber = orderNumber;
     }
 
+    /**
+     * Retrieves the date the order was placed.
+     *
+     * @return the order date
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * Sets the date the order was placed.
+     *
+     * @param date the new order date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
-    // toString-Methode
+    /**
+     * Returns a string representation of the Order.
+     * <p>
+     * Provides a human-readable form of the order entity, including its order number and date.
+     *
+     * @return a string detailing the order's information
+     */
     @Override
     public String toString() {
-        return "Auftrag{" +
-                "Auftragsnummer=" + orderNumber +
-                ", Datum=" + date +
+        return "Order{" +
+                "orderNumber=" + orderNumber +
+                ", date=" + date +
                 '}';
     }
 }
