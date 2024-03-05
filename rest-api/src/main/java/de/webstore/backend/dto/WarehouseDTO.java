53fe8d7d4b1c6b6879eb5c2e48bb0757587aa464
Davain Edwards <core8@gmx.net>
2024-03-05 21:21:02 +0100

    :up: Update WarehouseDTO

--- rest-api/src/main/java/de/webstore/backend/dto/WarehouseDTO.java
+++ rest-api/src/main/java/de/webstore/backend/dto/WarehouseDTO.java
@@ -1,19 +1,27 @@
 /* Copyright Davain Pablo Edwards core8@gmx.net. Licensed https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en */
 package de.webstore.backend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * Data Transfer Object (DTO) for Warehouse entities.
  * <p>
- * This class is used to transfer warehouse data, including warehouse number and quantity of products,
+ * This class is used to transfer warehouse data, including warehouse Number and quantity of products,
  * between processes or through network calls without exposing domain models.
  */
 public class WarehouseDTO {
     
-    // Unique identifier for the warehouse
+    // Number for the warehouse
+    @Schema(description = "Number for the warehouse", example = "1")
     private int warehouseNumber;
     
     // Quantity of products stored in the warehouse
+    @Schema(description = "Quantity of products stored in the warehouse", example = "500")
     private int quantity;
+
+    // Status of warehouse if active or not
+    @Schema(description = "Status of warehouse if active or not", example = "TRUE")
+    private boolean isActive;
 
     /**
      * Default constructor for creating an empty WarehouseDTO instance.
@@ -21,22 +29,24 @@ public class WarehouseDTO {
     public WarehouseDTO() {}
 
     /**
-     * Constructs a WarehouseDTO with a specified warehouse number and quantity.
+     * Constructs a WarehouseDTO with a specified warehouse ID and quantity.
      *
      * @param warehouseNumber the unique identifier of the warehouse
      * @param quantity the quantity of products in the warehouse
+     * @param isActive the status of the warehouse
      */
-    public WarehouseDTO(int warehouseNumber, int quantity) {
+    public WarehouseDTO(int warehouseNumber, int quantity, boolean isActive) {
         this.warehouseNumber = warehouseNumber;
         this.quantity = quantity;
+        this.isActive = isActive;
     }
 
     // Getters
     
     /**
-     * Retrieves the warehouse number.
+     * Retrieves the warehouse Number.
      *
-     * @return the unique identifier of the warehouse
+     * @return the number of the warehouse
      */
     public int getWarehouseNumber() {
         return warehouseNumber;
@@ -51,12 +61,20 @@ public class WarehouseDTO {
         return quantity;
     }
 
+    /**
+     * Retrieves the status of the warehouse.
+     * @return the status of the warehouse
+     */
+    public boolean getIsActive() {
+        return isActive;
+    }
+
     // Setters
     
     /**
-     * Sets the warehouse number.
+     * Sets the warehouse Number.
      *
-     * @param warehouseNumber the new unique identifier for the warehouse
+     * @param warehouseNumber the new number for the warehouse
      */
     public void setWarehouseNumber(int warehouseNumber) {
         this.warehouseNumber = warehouseNumber;
@@ -69,6 +87,15 @@ public class WarehouseDTO {
      */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    /**
+     * Sets the status of the warehouse.
+     * 
+     * @param isActive the new status of the warehouse
+     */
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
     }
 
     /**
