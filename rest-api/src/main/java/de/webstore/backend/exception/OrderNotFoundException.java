e4b93b76d2e7fd06c82b6305030f87011d844e17
Davain Edwards <core8@gmx.net>
2024-03-05 10:58:49 +0100

    :star: Add OrderNotFoundException class

---
+++ rest-api/src/main/java/de/webstore/backend/exception/OrderNotFoundException.java
@@ -0,0 +1,36 @@
+/* Copyright Davain Pablo Edwards core8@gmx.net. Licensed https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en */
+package de.webstore.backend.exception;
+
+/**
+ * Custom exception to be thrown when an order is not found.
+ */
+public class OrderNotFoundException extends RuntimeException {
+
+    /**
+     * Constructs a new OrderNotFoundException with the specified detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval by the getMessage() method).
+     */
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new OrderNotFoundException with the specified detail message and cause.
+     *
+     * @param message the detail message (which is saved for later retrieval by the getMessage() method).
+     * @param cause   the cause (which is saved for later retrieval by the getCause() method). (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
+     */
+    public OrderNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new OrderNotFoundException with the specified cause and a detail message of (cause==null ? null : cause.toString()) (which typically contains the class and detail message of cause).
+     *
+     * @param cause the cause (which is saved for later retrieval by the getCause() method). (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
+     */
+    public OrderNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
