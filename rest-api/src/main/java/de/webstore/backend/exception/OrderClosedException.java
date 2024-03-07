e1a3281ec711306bd43dde4f655d747b65d36d73
Davain Edwards <core8@gmx.net>
2024-03-07 22:59:02 +0100

    :up: Update OrderClosedException

--- rest-api/src/main/java/de/webstore/backend/exception/OrderClosedException.java
+++ rest-api/src/main/java/de/webstore/backend/exception/OrderClosedException.java
@@ -1,15 +1,10 @@
 /* Copyright Davain Pablo Edwards core8@gmx.net. Licensed https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en */
 package de.webstore.backend.exception;
 
-public class OrderClosedException extends Exception {
+public class OrderClosedException extends RuntimeException {
     
     // Constructor that accepts a message
     public OrderClosedException(String message) {
         super(message);
-    }
-
-    // Constructor that accepts a message and a cause
-    public OrderClosedException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
